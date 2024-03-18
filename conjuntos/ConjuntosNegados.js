b01d6db126d3e09239682a237bd733d1516d786e
adrielze <oadriel14@gmail.com>
2024-03-18 16:54:07 -0300

    feat: mais alterações nos conjuntos negados

--- conjuntos/ConjuntosNegados.js
+++ conjuntos/ConjuntosNegados.js
@@ -1,5 +1,6 @@
 const text = '1,2,3,a.b c!d?e[f'
 
+//o ^ retira todos os elementos presentes no conjunto que vêm seguido dele
 console.log(text.match(/\D/g));
 console.log(text.match(/[^0-9]/g));
 console.log(text.match(/[^\d!\?[\s,\.]/g));
